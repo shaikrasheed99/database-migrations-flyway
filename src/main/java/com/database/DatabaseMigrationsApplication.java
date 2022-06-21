f23308613164afe7b955a009f012360fd66008fa
rasheed <shaikrasheed3014@gmail.com>
2022-06-21 10:50:58 +0530

    Initial commit

---
+++ src/main/java/com/database/DatabaseMigrationsApplication.java
@@ -0,0 +1,13 @@
+package com.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMigrationsApplication.class, args);
+	}
+
+}
