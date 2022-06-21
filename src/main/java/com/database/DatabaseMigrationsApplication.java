e30d180fe8fca43f4dad826e386d5c6aea0298ca
rasheed <shaikrasheed3014@gmail.com>
2022-06-21 10:57:54 +0530

    Add postgresql configurations

--- src/main/java/com/database/DatabaseMigrationsApplication.java
+++ src/main/java/com/database/DatabaseMigrationsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DatabaseMigrationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatabaseMigrationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseMigrationsApplication.class, args);
+    }
 
 }
